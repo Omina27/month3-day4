543d178d7f718495a16c40f282ea05b2a65f38a0
omina0127 <abdusattorovaomina032@gmail.com>
2021-11-02 14:16:21 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,110 @@
+var studentName="Omina";
+console.log(typeof studentName, studentName);
+
+var studentSurname="Abdusattorova"
+console.log(typeof studentSurname, studentSurname);
+
+var studentAge=18;
+console.log(typeof studentAge, studentAge);
+
+var studentYear=1998;
+console.log(typeof studentYear, studentYear);
+
+var studentAddress="Tashkent";
+console.log(typeof studentAddress, studentAddress);
+
+var studentHobby;
+console.log(typeof studentHobby, studentHobby);
+
+var studentHeight=null;
+console.log(typeof studentHeight, studentHeight);
+
+var studentStudy=true;
+console.log(typeof studentStudy, studentStudy);
+
+var studentDream="Being a good programmist";
+console.log(typeof studentDream, studentDream);
+
+var studentNation="O'zbek";
+console.log(typeof studentNation, studentNation);
+
+var studentNumber=998941234567;
+console.log(typeof studentNumber, studentNumber);
+
+var studentUni="O'zbekistan Jahon tillari universiteti";
+console.log(typeof studentUni, studentUni);
+
+var studentlang="Ingliz tili";
+console.log(typeof studentlang, studentlang);
+
+var studentSco="6 band"
+console.log(typeof studentSco, studentSco);
+
+
+
+
+
+
+
+
+
+//promt
+var studentName=prompt("Ismingiz")
+//alert
+console.log(studentName);
+alert("O'quvchining ismi:" + " " + studentName);
+
+
+//promt
+var studentSurname=prompt("Familiyangiz")
+//alert
+console.log(studentSurname);
+alert("O'quvchining familiyasi:" + " " + studentSurname);
+
+//promt
+var studentAge=prompt("Yoshingiz")
+//alert
+console.log(studentAge);
+alert("O'quvchining yoshi:" + " " + studentAge);
+
+//promt
+var studentAddress=prompt("Manzilingiz")
+//alert
+console.log(studentAddress);
+alert("O'quvchining manzili:" + " " + studentAddress);
+
+//promt
+var studentDream=prompt("Maqsadi")
+//alert
+console.log(studentDream);
+alert("O'quvchining maqsadi:" + " " + studentDream);
+
+//promt
+var studentNation=prompt("Millati")
+//alert
+console.log(studentNation);
+alert("O'quvchining millati:" + " " + studentNation);
+
+var studentNumber=prompt("Telefon raqamingiz")
+//alert
+console.log(studentNumber);
+alert("O'quvchining raqami:" + " " + studentNumber);
+
+var studentUni=prompt("Ta'lim joyi")
+//alert
+console.log(studentUni);
+alert("O'quvchining universiteti:" + " " + studentUni);
+
+var studentlang=prompt("Qaysi tillarni bilasiz")
+//alert
+console.log(studentlang);
+alert("O'quvchining ikkinchitili:" + " " + studentlang);
+
+var studentSco=prompt("IELTS balingiz")
+//alert
+console.log(studentSco);
+alert("O'quvchining IELTS darajasi:" + " " + studentSco);
+
+
+
+
